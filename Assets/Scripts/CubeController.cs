9f1926718da5e771e41cbd444fbb6ca66d3459b5
yamaga <yamaga@wonderleague.co.jp>
2017-06-06 12:45:06 +0900

    Lesson7 課題提出
    Lesson7の２Dゲーム課題です。

---
+++ Assets/Scripts/CubeController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+	// キューブの移動速度
+	private float speed = -0.2f;
+
+	// 消滅位置
+	private float deadLine = -10;
+        AudioSource audioSource;
+
+        public AudioClip impact;
+
+        // 地面の位置
+        private float groundLevel = -3.0f;
+
+        private bool isGround = false;
+
+
+	// Use this for initialization
+	void Start () {
+                this.audioSource = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// キューブを移動させる
+		transform.Translate (this.speed, 0, 0);
+
+                 isGround = (transform.position.y > this.groundLevel) ? false : true;
+		
+		// 画面外に出たら破棄する
+		if (transform.position.x < this.deadLine){
+			Destroy (gameObject);
+		}
+		
+	}
+
+	void OnCollisionEnter2D (Collision2D other) {
+             if (other.gameObject.tag == "Ground") {
+                this.audioSource.Play();
+             }
+             if (other.gameObject.tag == "Block") {
+               this.audioSource.Play();
+               }
+	}
+
+}
